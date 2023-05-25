467fdff3b48e097366db90bad586ba12858ccbdc
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-05-25 22:37:42 +0300

    task_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,11 +4,11 @@ import { galleryItems } from './gallery-items.js';
 const galleryContainer = document.querySelector('.gallery');
 const cardsMarkup = createGalleryCardsMarkup(galleryItems);
 
-galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup)
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
 function createGalleryCardsMarkup(galleryItems) {
   return galleryItems
-    .map(({ preview, original, description }) => {
+    .map(({ preview, original, description } = {}) => {
       return `
     <li class="gallery__item">
         <a class="gallery__link" href="large-image.jpg">
@@ -25,11 +25,27 @@ function createGalleryCardsMarkup(galleryItems) {
     .join('');
 }
 
+const linkTargetBlank = document.querySelectorAll('.gallery__link');
+const linkTargetBlankArray = Array.prototype.slice.call(linkTargetBlank);
 
-// const linkTargetBlank = document.querySelectorAll('.gallery__link');
-// console.log(linkTargetBlank)
+linkTargetBlankArray.forEach((e) => {
+  e.addEventListener('click', noTargetBlank, false);
+})
 
-// function noTargetBlank(e) {
-//     e.preventDefault();
-// }
-// linkTargetBlank.addEventListener('click', noTargetBlank, false);
+function noTargetBlank(e) {
+  e.preventDefault();
+
+  const instance = basicLightbox.create(`
+    <div class="modal">
+          <img src = "${e.target.dataset.source}"/>
+    </div>
+`);
+  instance.show();
+
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      instance.close();
+    }
+  });
+  
+};
