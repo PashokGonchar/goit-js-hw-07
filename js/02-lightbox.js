0b784d47fe53b9f02a5bc3091d6dceeaef09a7c8
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-05-29 18:03:38 +0300

    second_task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description } = {}) => {
+      return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        </li>`;
+    })
+    .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+const linkTargetBlank = document.querySelectorAll('.gallery__link');
+const linkTargetBlankArray = Array.prototype.slice.call(linkTargetBlank);
+
+linkTargetBlankArray.forEach((e) => {
+  e.addEventListener('click', noTargetBlank, false);
+});
+
+function noTargetBlank(e) {
+  e.preventDefault();
+
+  const instance = lightbox.create(`
+  <div class="gallery">
+    <a href="${preview}"><img src="${original}" alt="${description}"/></a>
+  </div>
+`);
+  instance.show();
+
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      instance.close();
+    }
+  });
+}
